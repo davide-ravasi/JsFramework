041079446a7a7b1e9501b5ab55d5fcbdb0d7fc4e
davide-ravasi <info@davideravasi.com>
2014-06-05 11:53:01 +0200

    primo commit

---
+++ dr_personal_frameworkjs.js
@@ -0,0 +1,19 @@
+// data una funzione e un array esegue azione su el
+//dipendenze: no
+function forEach(array, action) { 
+  for (var i = 0; i < array.length; i++)
+    action(array[i]);
+}
+
+//dato array  e valore lo cerca ritornando bool
+//dipendenze forEach
+function IsArrMember(array, value) {
+  var found = false;
+  forEach(array, function(element) {
+    if (element === value)
+      found = true;
+  });
+ return found;
+}
+
+//IsArrMember([6, 7, "Bordeaux"], 6);
